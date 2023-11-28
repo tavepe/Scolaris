18f4a810de35d7ac1bdfce1775ce6fa51f604237
Gustavo Zanin <taveppe23@gmail.com>
2023-11-28 19:38:11 -0300

    Criação do projeto JAVA
    Foi criado o projeto JAVA com as dependências que serão necessárias para o projeto web.

---
+++ src/main/java/com/senac/Scolaris/ScolarisApplication.java
@@ -0,0 +1,13 @@
+package com.senac.Scolaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScolarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScolarisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/Scolaris/ScolarisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.Scolaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScolarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
