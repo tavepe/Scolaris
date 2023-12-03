f5f0cbe8e0f8c9f97c864a802af4355bb854e18b
Gustavo Zanin <taveppe23@gmail.com>
2023-12-03 02:46:49 -0300

    Criado notas Entity e Repository

---
+++ src/main/java/com/senac/Scolaris/data/NotasEntity.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senac.Scolaris.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ *
+ * @author tavep
+ */
+@Data
+@Entity
+@Table(name = "notas")
+public class NotasEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer alunoId;
+    private String professorId;
+    private String nota;
+    private String recado;
+}
