7a34e29493f066c5ae279b472ca803be9dee55d9
Gustavo Zanin <taveppe23@gmail.com>
2023-12-03 02:21:35 -0300

    Criado avaliacoes Entity e Repository

---
+++ src/main/java/com/senac/Scolaris/data/AvaliacoesEntity.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senac.Scolaris.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ *
+ * @author tavep
+ */
+@Data
+@Entity
+@Table(name="Avaliacoes")
+public class AvaliacoesEntity {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private Integer turmaId;
+    private String titulo;
+    private String valorTotal;
+    private String descricao;
+}
